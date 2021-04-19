da0c6435855d6ebfbe43876009b63ed9c4780ae9
Aizhan Oshurakhunova <xoshur00@vutbr.cz>
2021-04-19 10:50:58 +0200

    add liner search

--- searching.py
+++ searching.py
@@ -1,7 +1,6 @@
 import os
 import json
 
-# get current working directory path
 cwd_path = os.getcwd()
 
 
@@ -22,9 +21,24 @@ def read_data(file_name, field):
     return data[field]
 
 
+def linear_search(sequence, number):
+    indices = []
+    count = 0
+    idx = 0
+    while idx < len(sequence):
+        if sequence[idx] == number:
+            count = count + 1
+            indices.append(idx)
+        idx = idx + 1
+
+    return {"positions": indices, "count": count}
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
-    print(sequential_data)
+    my_number = 10
+    results = linear_search(sequential_data, my_number)
+    print(results)
 
 
 if __name__ == '__main__':
