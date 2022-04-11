55067f24ac87aff890c7d74e394df9b8aa76bc9f
Dušan Kichner <xkichn00@vutbr.cz>
2022-04-11 09:21:23 +0200

    'random'

--- searching.py
+++ searching.py
@@ -1,8 +1,22 @@
+import json
 import os
 
 # get current working directory path
 cwd_path = os.getcwd()
 
+def linear_search(sequence,number_look_for):
+    """
+    :param sequence:
+    :param number_look_for(int):
+    :return:
+    """
+    count = 0
+    indicie = list()
+    for index,num in enumerate(sequence):
+        if num == number_look_for:
+            indicie.append(index)
+            count += 1
+    return {'pozice': indicie, 'count':count}
 
 def read_data(file_name, field):
     """
@@ -11,10 +25,20 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    if field not in {'unordered_numbers','ordered_numbers','dna_sequence'}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
 
+    with open(file_path,'r') as json_file:
+        seq = json.load(json_file)
+    return seq[field]
 
 def main():
+    sequential_data = read_data('sequential.json', 'unordered_numbers')
+    print(sequential_data)
+    linear = linear_search(sequential_data, 9)
+    print(linear)
     pass
 
 
